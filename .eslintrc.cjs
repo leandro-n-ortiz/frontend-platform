34379950cbe532d1f76122f449742d2c2ac6189e
Leandro Ortiz <leandrortiz@hotmail.com>
2023-04-22 16:50:58 -0300

    ESList and Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,21 @@
+/*eslint-env node*/ // ESLint runs on node env, so it can have the underclared module here
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true, // This is the application's env (not ESList's env)
+    es2020: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -11,4 +23,4 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
-}
+};
